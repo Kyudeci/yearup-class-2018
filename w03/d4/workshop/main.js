a9091482059d411148f8283d5c5806c36d0c5222
Shakiem Johnson <sjohnson@nyc.yearup.org>
2018-11-08 16:54:21 -0500

    d4 workshop

--- w03/d4/workshop/main.js
+++ w03/d4/workshop/main.js
@@ -1,3 +1,11 @@
 //This comes from data.js
 var gifs = window.data.data;
 
+function ranGif() {
+  let gifElement = document.getElementById('gifs');
+  let randomNumber = Math.floor(Math.random() * gifs.length);
+  gifElement.src = gifs[randomNumber].images.original.url;
+}
+
+let button = document.querySelector('button');
+button.addEventListener('click', ranGif);
