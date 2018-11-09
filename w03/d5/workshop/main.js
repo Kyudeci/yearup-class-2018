70ab59fc36a256f490d1093b423323da5b5d123d
Shakiem Johnson <sjohnson@nyc.yearup.org>
2018-11-09 12:57:14 -0500

    First commit

--- w03/d5/workshop/main.js
+++ w03/d5/workshop/main.js
@@ -1,16 +1,20 @@
-class Art {
-  constructor(){
-
+class Classic {
+  constructor(art_url){
+    // this.frame = frame
+    this.art_url = art_url;
   }
-
   render(){
     //do things to the DOM here
+    let imgEl = document.querySelector("img")
+    imgEl.src = art_url.images.original.url
   }
 }
+let art_url =
+// class Classical extends Art {
+//   constructor(){
+//     super();
+//   }
+//
+// }
 
-class Classical extends Art {
-  constructor(){
-    super();
-  }
-
-}
+new Classic("http://simpleabstract.com/assets/images/Oil-On-Canvas-Abstract-Art.jpg")
